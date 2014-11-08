102224ea21ae85af32ce983d0a086fbf704a0893
irbowen <irbowen@umich.edu>
2014-11-08 14:47:20 -0500

    Update main.cpp

--- release/main.cpp
+++ release/main.cpp
@@ -13,3 +13,17 @@ int main(int argc, char* argv[]) {
 	gameT.join();
 	return 0;
 }
+
+// <param name="hInstance">handle to the application instance</param>
+/// <param name="hPrevInstance">always 0</param>
+/// <param name="lpCmdLine">command line arguments</param>
+/// <param name="nCmdShow">whether to display minimized, maximized, or normally</param>
+/// <returns>status</returns>
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	std::thread glThread(glDriver);
+	std::thread gameThread(startGame);
+	gameThread.join();
+	glThread.join();
+	return 0;
+}
