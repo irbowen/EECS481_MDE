cc66a88d6e6597aae4e70801362202806a5dd092
irbowen <irbowen@umich.edu>
2014-11-08 14:31:18 -0500

    Update game.h

--- release/game.h
+++ release/game.h
@@ -1,6 +1,3 @@
-//Isaac Bowen
-//eecs481 fall2014
-
 #include <vector>
 #include <chrono>
 #include <thread>
@@ -11,17 +8,15 @@ class Game {
 	const int NUM_ROUNDS = 10;
 	const int INCREASE_FACTOR = 1;
 	const int BREAK_FACTOR = 20;
-	const int MAX_X = 100;
-	const int MAX_Y = 100;
+	const int MAX_X = 640;
+	const int MAX_Y = 1480;
 	const int MAX_NUM_SPOTS = 10;
 	const int SAMPLE_MILLISECONDS = 100;
-	const int POINTS_PER_CORRECT = 100;
 	double start_radius = 5;
 	int num_active_spots = 0;
 	std::vector<Location> loc_list;
 	Location createRandomLocation();
 public:
-	bool input = false;
 	Game();
 	void run();
 	void startGame();
