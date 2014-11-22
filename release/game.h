c0c468f6156821af6b7ed2a7a06920b0ce38b649
lee-CS <john.flo.lee@gmail.com>
2014-11-22 14:07:01 -0500

    minor change

--- release/game.h
+++ release/game.h
@@ -20,13 +20,8 @@ class Game {
 	const int MAX_NUM_SPOTS = 3;
 	const int SAMPLE_MILLISECONDS = 100;
 	double start_radius = 20;
-<<<<<<< HEAD
 	int num_active_spots = 0;
 	Location createRandomLocation(int opt_x, int opt_y);
-=======
-	int num_active_spots = 0, num_triggered_spots = 0;
-	Location createRandomLocation(int opt_x = -1, int opt_y = -1);
->>>>>>> origin/master
 	LocPair createRandomLocPair(int opt_x1 = -1, int opt_y1 = -1, int opt_x2 = -1, int opt_y2 = -1);
 	double checkPressure(Location loc);
 	CDepthBasics* kinect;
