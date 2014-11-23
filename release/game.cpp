89326451e0541a1a4ce935a4d0ea70996c4b17a2
lee-CS <john.flo.lee@gmail.com>
2014-11-23 17:54:00 -0500

    LocPair compiles with fixed structure

--- release/game.cpp
+++ release/game.cpp
@@ -152,6 +152,63 @@ void Game::runSlideRingMode(int i) {
 	LocationLock.unlock();
 }
 
+double Game::dist(int x1, int y1, int x2, int y2)
+{
+	int dist_x = x2 - x1;
+	int dist_y = y2 - y1;
+
+	double length = sqrt((dist_x*dist_x) + (dist_y*dist_y));
+
+	return length;
+}
+
+bool Game::on_line(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+	if ((dist(x3, y3, x1, y1) + dist(x3, y3, x2, y2) - dist(x1, y1, x2, y2)) / dist(x1, y1, x2, y2) < 0.1)
+		return true;
+	else
+		return false;
+}
+
+bool Game::line(LocPair& locpair)
+{
+	const int MAX_X = 640;
+	const int MAX_Y = 480;
+	ColorSlideRing loc1 = locpair.start;
+	ColorSlideRing loc2 = locpair.destination;
+	int last_x = (int)loc1.getX();
+	int last_y = (int)loc1.getY();
+	int x1 = (int)loc1.getX();
+	int y1 = (int)loc1.getY();
+	int x2 = (int)loc2.getX();
+	int y2 = (int)loc2.getY();
+
+	// draw current track and check whether the line is finished
+	while (dist(last_x, x2, last_y, y2) - (int)loc2.getR()>0){
+		// Assume minDepth is the cursor
+
+		//get cursor location (400,400) is example for now
+		int cursor_x = 400;
+		int cursor_y = 400;
+
+		int x = cursor_x % MAX_X; // column
+		int y = cursor_y / MAX_X; // row
+
+		if (on_line(x1, y1, x2, y2, x, y))
+		{
+			//if(dist(x,y,x2,y2)<dist(last_x,last_y,x2,y2)){
+			last_x = x;
+			last_y = y;
+			//OpenGL draw line
+			Scene::lines.push_back({ { x1, y1 }, { last_x, last_y }, RED, 5.0 });
+			//}
+		}
+		else
+			return false;
+	}
+	return true;
+}
+
 
 void Game::runConnectMode()
 {
@@ -181,7 +238,7 @@ void Game::runConnectMode()
 		if (Scene::locpair.locked == true) 
 		{
 			int i = 0;
-			while (Scene::locpair.line())
+			while (line(Scene::locpair))
 			{
 				if (i++ > 10) break;
 			}
