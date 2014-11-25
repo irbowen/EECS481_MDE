74f3d39a4fe4b89b49b7e861cd6c5e3c6eb2f56d
lee-CS <john.flo.lee@gmail.com>
2014-11-24 23:25:31 -0500

    minor

--- release/LocPair.h
+++ release/LocPair.h
@@ -1,6 +1,7 @@
 #ifndef LOCPAIR_H
 #define LOCPAIR_H
 
+#include "location.h"
 #include "Color.h"
 #include "ColorSlideRing.h"
 
@@ -9,25 +10,16 @@
 class LocPair{
 public:
 	LocPair();
-	LocPair(double, double, double, double, double, double);
+	LocPair(double, double, double, double, double, double, double);
 
 	bool locked = false;
 	//bool on;
 	double TARGET_PRESSURE = 500;
 	double pressure;
-	ColorSlideRing start;
-	ColorSlideRing destination;
-	Color start_color;
-	Color dest_color;
-	void start_setPressure(double);
-	double start_getPercentage(double);
-	void dest_setPressure(double);
-	double dest_getPercentage(double);
-	double rStart;
-	double start_pressure;
+	Location start;
+	Location end;
 	void draw();
 	//bool line();
-	bool withinPressure(double input);
 	//double dist(int x1, int y1, int x2, int y2);
 	//bool on_line(int x1, int y1, int x2, int y2, int x3, int y3);
 };
